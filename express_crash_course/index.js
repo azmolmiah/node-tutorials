8426b790bf2207bc5f75bdac8c92acde490918de
Azmol-Miah <azmol@azmolmiah.com>
2019-11-17 13:46:46 +0000

    completed express crash course

--- express_crash_course/index.js
+++ express_crash_course/index.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const path = require("path");
-const members = require("./Members");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const members = require("./Members");
 
 const app = express();
 
 // Init middleware
-app.use(logger);
+//app.use(logger);
 
-// Get all members
-app.get("/api/members", (req, res) => res.json(members));
+// Handle bars middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members
+  })
+);
+
+// Body parser middlewar
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Set a static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
